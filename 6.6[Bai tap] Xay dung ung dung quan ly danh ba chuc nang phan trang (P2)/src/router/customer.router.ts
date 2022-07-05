e9c5443059ad53e93ef9fe30f7b5b3a7539cf9f2
HaiTrongDo <dotronghai91@gmail.com>
2022-07-05 14:49:36 +0700

    update

---
+++ 6.6[Bai tap] Xay dung ung dung quan ly danh ba chuc nang phan trang (P2)/src/router/customer.router.ts
@@ -0,0 +1,83 @@
+import {Router} from 'express';
+const customerRoutes = Router();
+import {Customer} from "../schemas/customer.model";
+import multer from 'multer'
+
+const upload = multer();
+customerRoutes.get('/create', (req, res) => {
+    res.render("createCustomer");
+});
+
+customerRoutes.post('/create', upload.none(), async (req, res) => {
+    try {
+        const newCustomer = new Customer(req.body);
+        const customer = await newCustomer.save();
+        if (customer) {
+            res.render("success");
+        } else {
+            res.render("error");
+        }
+    } catch (err) {
+        res.render("error");
+    }
+});
+
+customerRoutes.post('/update', upload.none(), async (req, res) => {
+    try {
+        const customer = await Customer.findOne({_id: req.body.id});
+        customer.customerName = req.body.title;
+        customer.phone = req.body.description;
+        customer.address = req.body.author;
+        await customer.save();
+        if (customer) {
+            res.render("success");
+        } else {
+            res.render("error");
+        }
+    } catch (err) {
+        res.render("error");
+    }
+});
+
+customerRoutes.get('/list', async (req, res) => {
+    try {
+        let limit: number = 3;
+        let offset = !req.query.offset ? 0: parseInt(req.query.offset as string)
+        const customers = await Customer
+                                    .find()
+                                    .limit(limit)
+                                    .skip(limit*offset)
+        res.render("listCustomer", {customers: customers});
+    } catch {
+        res.render("error");
+    }
+});
+
+customerRoutes.get('/update/:id', async (req, res) => {
+    try {
+        const customer = await Customer.findOne({_id: req.params.id});
+        console.log(customer, 'customer')
+        if (customer) {
+            res.render("updateCustomer", {customer: customer})
+        } else {
+            res.render("error");
+        }
+    } catch (err) {
+        res.render("error");
+    }
+});
+
+customerRoutes.delete('/delete/:id', async (req, res) => {
+    try {
+        const customer = await Customer.findOne({_id: req.params.id});
+        if (customer) {
+            await customer.remove();
+            res.status(200).json({message: "success"});
+        } else {
+            res.render("error");
+        }
+    } catch (err) {
+        res.render("error");
+    }
+});
+export default customerRoutes;
